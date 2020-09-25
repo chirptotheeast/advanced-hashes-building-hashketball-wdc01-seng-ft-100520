2d711ba6b836036db7dde7d49839bf27e8c93783
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-25 18:58:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,8 @@ require 'pry'
 def game_hash 
   
   
-  {:home => {:team_name => '',
-            :colors => [], 
+  {:home => {:team_name => 'Brooklyn Nets',
+            :colors => ['Black', 'White'], 
             :players => [{:player_name => "Alan Anderson",
                           :number => 0, 
                           :shoe => 16,
